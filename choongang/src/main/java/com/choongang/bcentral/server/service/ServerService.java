03a2e7725342745b77ed1ce8221a87b5488bb9aa
JJ <JJ@LAPTOP-NP0K1FDD>
2022-04-01 13:00:16 +0900

    서버관리 오류 수정

--- choongang/src/main/java/com/choongang/bcentral/server/service/ServerService.java
+++ choongang/src/main/java/com/choongang/bcentral/server/service/ServerService.java
@@ -7,6 +7,7 @@ import java.time.LocalTime;
 import java.time.temporal.TemporalField;
 import java.time.temporal.WeekFields;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import com.choongang.bcentral.mapper.ServerSQLMapper;
 import com.choongang.bcentral.schedule.vo.ScheduleVo;
 import com.choongang.bcentral.server.vo.PageVo;
 import com.choongang.bcentral.server.vo.ServerVo;
+import com.google.gson.Gson;
 
 // [서버관리] Service
 @Service
@@ -81,7 +83,7 @@ public class ServerService {
 	      int day = now.getDayOfWeek().getValue() - 1; 
 	      
 	      switch(sVo.getRepeat_cat()) {
-	      case 0:
+	      case 0: //매일하는 작업일 경우
 	         if(now_date.isEqual(sVo.getStart_date()) && now_time.isBefore(sVo.getStart_time())) {
 	            result = 0;
 	         } else if(now_date.isEqual(sVo.getEnd_date()) && now_time.isAfter(sVo.getEnd_time())) {
@@ -90,7 +92,7 @@ public class ServerService {
 	            result = 1;
 	         }
 	         break;
-	      case 1:
+	      case 1: //특정 요일의 작업일 경우
 	         if(days[day] != null) { //오늘요일에 스케줄이 있으면
 	            if(now_time.isBefore(sVo.getStart_time())) {
 	               result = 0;
@@ -101,7 +103,7 @@ public class ServerService {
 	            }
 	         }
 	         break;
-	      case 2: //특정 주차의 특정 요일
+	      case 2: //특정 주차의 특정 요일의 작업일 경우
 	    	 
 	    	  //아마도 틀 인듯? 한주의 시작을 일요일, 달도 그런 일주일 형식으로 설정?
 	         WeekFields weekFields = WeekFields.of(DayOfWeek.SUNDAY, 1); //(DayOfWeek firstDayOfWeek, int minimalDaysInFirstWeek?)
@@ -120,7 +122,7 @@ public class ServerService {
 	            }
 	         }
 	         break;
-	      case 3: //매달 특정일
+	      case 3: //매달 특정일에 하는 작업일 경우
 	         if(sVo.getRepeat_day() == now.getDayOfMonth()) { //특정일이 오늘 일과 같으면 
 	            if(now_time.isBefore(sVo.getStart_time())) {
 	               result = 0;
@@ -132,7 +134,8 @@ public class ServerService {
 	         }
 	         break;
 	      }
-	      
+	      System.out.println(new Gson().toJson(sVo));
+	      System.out.println(now + "현재 작업 상태는" + result);
 	      return result;
 	   }
 
@@ -144,11 +147,118 @@ public class ServerService {
 		return svSQLMapper.getScheduleByScheduleNo(sc_no);
 	}
 	
+	public ArrayList<Integer> getServerListByServerNo(int sc_no) {
+		
+		return svSQLMapper.getServerNosByScNo(sc_no);
+	}
+	
+	public ArrayList<HashMap<String, Object>> todayScheduleState(){
+		ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String,Object>>(); 
+				
+		ArrayList<ScheduleVo> todayScList= svSQLMapper.selectTodaySchedule();
+ 		for(ScheduleVo sVo : todayScList) {
+ 	 		HashMap<String, Object> map = new HashMap<String, Object>();
+ 			int sc_no = sVo.getSc_no();
+ 			int status = todaySchedule(sVo);
+ 			ArrayList<Integer> serverNos = svSQLMapper.getServerNosByScNo(sc_no);
+ 			 			
+ 			map.put("sc_no", sc_no);
+ 			map.put("status", status);
+ 			map.put("serverNos", serverNos);
+
+ 			int state = 3;
+ 			for(int serverNo : serverNos) {
+ 				ArrayList<Integer> scNos = svSQLMapper.getScNoListByServerNo(serverNo);
+ 				ArrayList<Integer> stateList = new ArrayList<Integer>();
+
+ 				if(scNos == null) {
+ 					state = 3;
+ 				} else {
+ 					for(int scNo : scNos) {
+ 						ScheduleVo vo= getScheduleByScNo(scNo);
+ 						int s = todaySchedule(sVo);
+ 						System.out.println(serverNo + "번 서버의 "+scNo + "번 스케줄의 현재 작업상태 :" +   s);
+ 						stateList.add(s);	
+ 					}
+ 					
+ 					if(stateList.contains(1)) {
+ 						state  = 1;
+ 					} else if(stateList.contains(0)) {
+ 						state = 0;
+ 					} else if(stateList.contains(2)) {
+ 						state = 2;
+ 					} else {
+ 						state = 3;
+ 					}
+ 				}
+ 		
+ 				
+ 				
+ 			}
+ 			
+ 		}
+
+		return list;
+	}
+	
+	
+//	public 
+//	ArrayList<HashMap<String, Object>> list = todayScheduleState();
+//	
+//	for(HashMap<String, Object> map : list) {
+//		if(map.get("serverNos") != null) {
+//			ArrayList<Integer> nos = (ArrayList<Integer>) map.get("serverNos");
+//
+//		}
+//		
+//		
+//	}
+//	
 	
 	
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public int getServerState(int server_no){
+		int state = 3;
+		ArrayList<Integer> scNos= svSQLMapper.getScNoListByServerNo(server_no);
+		ArrayList<Integer> stateList = new ArrayList<Integer>();
+
+		if(scNos == null) {
+			state = 3;
+		} else {
+			for(int no : scNos) {
+				ScheduleVo sVo= getScheduleByScNo(no);
+				int s = todaySchedule(sVo);
+				System.out.println(server_no + "번 서버의 "+ no + "번 스케줄의 현재 작업상태 :" +   s);
+				stateList.add(s);	
+			}
+
+			if(stateList.contains(1)) {
+				state  = 1;
+			} else if(stateList.contains(0)) {
+				state = 0;
+			} else if(stateList.contains(2)) {
+				state = 2;
+			} else {
+				state = 3;
+			}
+		}
+
+		return state;
+	}
 	
 	
 	
